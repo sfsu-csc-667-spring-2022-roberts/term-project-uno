f997a84d6065d34114d458550f2f7e2abcfa49f2
Switch24-7 <stephenjusto24@gmail.com>
2022-03-07 21:26:05 -0800

    remove dialectOptions

--- config/config.js
+++ config/config.js
@@ -4,22 +4,13 @@ module.exports = {
   "development": {
     "use_env_variable": "DATABASE_URL",
     "dialect": "postgres",
-    "dialectOptions": {
-      "ssl": true,
-    }
   },
   "test": {
     "use_env_variable": "DATABASE_URL",
     "dialect": "postgres",
-    "dialectOptions": {
-      "ssl": true,
-    }
   },
   "production": {
     "use_env_variable": "DATABASE_URL",
     "dialect": "postgres",
-    "dialectOptions": {
-      "ssl": true,
-    }
   }
 }
