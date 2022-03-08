21401bac7dbf12939ec67458d6408021d87b106b
Switch24-7 <stephenjusto24@gmail.com>
2022-03-07 21:24:57 -0800

    fix issues

--- db/index.js
+++ db/index.js
@@ -1,4 +1,16 @@
 const pgp = require('pg-promise')();
-const connection = pgp(process.env.DATABASE_URL + "?ssl=true");
+const { ConnectionString } = require('connection-string');
+const cnObj = new ConnectionString(process.env.DATABASE_URL);
+const connection = {
+  host: cnObj.hostname,
+  port: cnObj.port,
+  database: cnObj.path?.[0],
+  user: cnObj.user,
+  password: cnObj.password,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+};
+const db = pgp(connection);
 
-module.exports = connection;
+module.exports = db;
