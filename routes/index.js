23f7081f0e1ec6f6d5277d1517f9866875032037
Switch24-7 <stephenjusto24@gmail.com>
2022-03-07 20:52:55 -0800

    fix spacing

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,14 @@ const db = require('../db/index.js');
 
 /* GET home page. */
 router.get("/", (request, response) => {
-  db.any(`INSERT INTO test_table ("testString") VALUES ('Hello at $
-  {Date.now()}')`)
+  db.any(`INSERT INTO test_table ("testString") VALUES ('Hello at ${Date.now()}')`)
   .then( _ => db.any(`SELECT * FROM test_table`) )
   .then( results => response.json( results ) )
   .catch( error => {
     console.log( error )
     response.json({ error })
   })
- });
+});
 
  module.exports = router;
  
