09bdc61dda0f56bd1782f7c50e3a18461c21cc10
Switch24-7 <stephenjusto24@gmail.com>
2022-03-07 21:14:17 -0800

    add dialectOptions

--- config/config.js
+++ config/config.js
@@ -3,17 +3,30 @@ require('dotenv').config();
 module.exports = {
   "development": {
     "use_env_variable": "DATABASE_URL",
-    "dialect": "postgres"
+    "dialect": "postgres",
+    "dialectOptions": {
+      "ssl": {
+        "ssl": true,
+        "rejectUnauthorized": false
+      }
+    }
   },
   "test": {
     "use_env_variable": "DATABASE_URL",
-    "dialect": "postgres"
+    "dialect": "postgres",
+    "dialectOptions": {
+      "ssl": {
+        "ssl": true,
+        "rejectUnauthorized": false
+      }
+    }
   },
   "production": {
     "use_env_variable": "DATABASE_URL",
     "dialect": "postgres",
     "dialectOptions": {
       "ssl": {
+        "ssl": true,
         "rejectUnauthorized": false
       }
     }
