226d468afc9d165b8d7c0bfc720da58e966e5162
Switch24-7 <stephenjusto24@gmail.com>
2022-03-28 12:16:06 -0700

    fix spacing and simplify db connection

--- db/index.js
+++ db/index.js
@@ -1,16 +1,4 @@
 const pgp = require('pg-promise')();
-const { ConnectionString } = require('connection-string');
-const cnObj = new ConnectionString(process.env.DATABASE_URL);
-const connection = {
-  host: cnObj.hostname,
-  port: cnObj.port,
-  database: cnObj.path?.[0],
-  user: cnObj.user,
-  password: cnObj.password,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-};
-const db = pgp(connection);
+const connection = pgp(process.env.DATABASE_URL);
 
-module.exports = db;
+module.exports = connection;
--- migrations/20220308043046-first-migration.js
+++ migrations/20220308043046-first-migration.js
@@ -2,25 +2,25 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-  return queryInterface.createTable(
-  'test_table',
-  {
-  id: {
-  type: Sequelize.INTEGER,
-  primaryKey: true,
-  autoIncrement: true
-  },
-  createdAt: {
-  type: Sequelize.DATE,
-  defaultValue: Sequelize.literal('NOW()'),
-  allowNull: false
-  },
-  testString: {
-  type:Sequelize.STRING,
-  allowNull: false
-  }
-  }
-  );
+    return queryInterface.createTable(
+      'test_table',
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        createdAt: {
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal('NOW()'),
+          allowNull: false
+        },
+        testString: {
+          type:Sequelize.STRING,
+          allowNull: false
+        }
+      }
+    );
   },
   down: (queryInterface, Sequelize) => { 
     return queryInterface.dropTable('test_table');
