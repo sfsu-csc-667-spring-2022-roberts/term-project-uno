99cf987426bb68f7b82988ebbbe316316147a300
Switch24-7 <stephenjusto24@gmail.com>
2022-04-04 15:42:01 -0700

    update db connection

--- db/index.js
+++ db/index.js
@@ -1,4 +1,23 @@
 const pgp = require('pg-promise')();
-const connection = pgp(process.env.DATABASE_URL);
 
-module.exports = connection;
+if (process.env.NODE_ENV === 'production') {
+  const { ConnectionString } = require('connection-string');
+  const cnObj = new ConnectionString(process.env.DATABASE_URL);
+  const connection = {
+    host: cnObj.hostname,
+    port: cnObj.port,
+    database: cnObj.path?.[0],
+    user: cnObj.user,
+    password: cnObj.password,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  };
+  const db = pgp(connection);
+
+  module.exports = db;
+} else {
+  const connection = pgp(process.env.DATABASE_URL);
+
+  module.exports = connection;
+}
