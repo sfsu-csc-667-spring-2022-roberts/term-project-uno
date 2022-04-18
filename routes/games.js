5d22d2086f06b4e408a8c0c7dd477a0ccdaa8b73
iamfaisalh <faisalhakimi101@gmail.com>
2022-04-17 17:07:56 -0700

    modified card layout and designed special/wild cards

--- routes/games.js
+++ routes/games.js
@@ -15,55 +15,74 @@ router.post('/', authenticate, async (req, res) => {
     }
 });
 
+const gameState = {
+    players: [
+        { id: 1, userID: 5, turnIndex: 1, username: "saggyballs", cards: 1, avatar: "https://store.playstation.com/store/api/chihiro/00_09_000/container/CH/de/99/EP2402-CUSA05624_00-AV00000000000213/0/image?_version=00_09_000&platform=chihiro&bg_color=000000&opacity=100&w=330&h=330" },
+        { id: 2, userID: 1, turnIndex: 0, username: "iamfaisalh", cards: 7, avatar: "https://store.playstation.com/store/api/chihiro/00_09_000/container/CH/de/99/EP2402-CUSA05624_00-AV00000000000213/0/image?_version=00_09_000&platform=chihiro&bg_color=000000&opacity=100&w=330&h=330" },
+        { id: 3, userID: 222, turnIndex: 2, username: "coolguy905", cards: 30, avatar: "https://store.playstation.com/store/api/chihiro/00_09_000/container/CH/de/99/EP2402-CUSA05624_00-AV00000000000213/0/image?_version=00_09_000&platform=chihiro&bg_color=000000&opacity=100&w=330&h=330" },
+        { id: 4, userID: 319, turnIndex: 3, username: "joemomma", cards: 1, avatar: "https://store.playstation.com/store/api/chihiro/00_09_000/container/CH/de/99/EP2402-CUSA05624_00-AV00000000000213/0/image?_version=00_09_000&platform=chihiro&bg_color=000000&opacity=100&w=330&h=330" },
+        { id: 5, userID: 401, turnIndex: 9, username: "horse666fivefive", cards: 1, avatar: "https://store.playstation.com/store/api/chihiro/00_09_000/container/CH/de/99/EP2402-CUSA05624_00-AV00000000000213/0/image?_version=00_09_000&platform=chihiro&bg_color=000000&opacity=100&w=330&h=330" },
+        { id: 6, userID: 282, turnIndex: 4, username: "nike10", cards: 1, avatar: "https://store.playstation.com/store/api/chihiro/00_09_000/container/CH/de/99/EP2402-CUSA05624_00-AV00000000000213/0/image?_version=00_09_000&platform=chihiro&bg_color=000000&opacity=100&w=330&h=330" },
+        { id: 7, userID: 808, turnIndex: 6, username: "tax", cards: 1, avatar: "https://store.playstation.com/store/api/chihiro/00_09_000/container/CH/de/99/EP2402-CUSA05624_00-AV00000000000213/0/image?_version=00_09_000&platform=chihiro&bg_color=000000&opacity=100&w=330&h=330" },
+        { id: 8, userID: 913, turnIndex: 7, username: "king707", cards: 2, avatar: "https://store.playstation.com/store/api/chihiro/00_09_000/container/CH/de/99/EP2402-CUSA05624_00-AV00000000000213/0/image?_version=00_09_000&platform=chihiro&bg_color=000000&opacity=100&w=330&h=330" },
+        { id: 9, userID: 300, turnIndex: 8, username: "glizzy", cards: 28, avatar: "https://store.playstation.com/store/api/chihiro/00_09_000/container/CH/de/99/EP2402-CUSA05624_00-AV00000000000213/0/image?_version=00_09_000&platform=chihiro&bg_color=000000&opacity=100&w=330&h=330" },
+        { id: 10, userID: 552, turnIndex: 5, username: "flowerboy1", cards: 59, avatar: "https://store.playstation.com/store/api/chihiro/00_09_000/container/CH/de/99/EP2402-CUSA05624_00-AV00000000000213/0/image?_version=00_09_000&platform=chihiro&bg_color=000000&opacity=100&w=330&h=330" },
+    ],
+    playedDeck: [
+        { id: 44, color: "red", value: 3, special: false },
+        { id: 8, color: "yellow", value: 7, special: false },
+        { id: 43, color: "blue", value: 7, special: false },
+        { id: 56, color: "red", value: 4, special: false },
+        { id: 1, color: "green", value: 0, special: false },
+        { id: 840, color: "wild", value: "choose", special: true },
+        { id: 849, color: "wild", value: "plus4choose", special: true },
+        { id: 700, color: "green", value: "skip", special: true },
+        { id: 56, color: "red", value: 1, special: false },
+        { id: 720, color: "yellow", value: "plus2", special: true },
+        { id: 740, color: "green", value: "reverse", special: true },
+    ],
+    mainDeck: [
+        { id: 200, color: "green", value: 2, special: false },
+        { id: 201, color: "yellow", value: 0, special: false },
+        { id: 202, color: "red", value: 0, special: false },
+        { id: 203, color: "red", value: 9, special: false },
+        { id: 204, color: "green", value: 1, special: false },
+        { id: 205, color: "blue", value: 6, special: false },
+        { id: 206, color: "red", value: 3, special: false },
+        { id: 207, color: "yellow", value: 7, special: false },
+        { id: 208, color: "blue", value: 7, special: false },
+        { id: 209, color: "red", value: 4, special: false },
+        { id: 210, color: "green", value: 0, special: false },
+        { id: 211, color: "blue", value: 2, special: false },
+        { id: 212, color: "yellow", value: 0, special: false },
+        { id: 213, color: "blue", value: 0, special: false },
+        { id: 701, color: "red", value: "plus2", special: true },
+        { id: 702, color: "green", value: "plus2", special: true },
+        { id: 703, color: "blue", value: "plus2", special: true },
+        { id: 711, color: "red", value: "skip", special: true },
+        { id: 712, color: "yellow", value: "skip", special: true },
+        { id: 713, color: "blue", value: "skip", special: true },
+        { id: 842, color: "wild", value: "choose", special: true },
+        { id: 841, color: "wild", value: "choose", special: true },
+        { id: 740, color: "red", value: "reverse", special: true },
+        { id: 740, color: "blue", value: "reverse", special: true },
+        { id: 880, color: "wild", value: "plus4choose", special: true },
+        { id: 881, color: "wild", value: "plus4choose", special: true },
+    ],
+    drawDeckCount: 50,
+    turnIndex: 0,
+    currentColor: "red",
+    playerOrderReversed: false,
+};
+
 /* Get game state */
 router.get('/:id', authenticate, async (req, res) => {
     try {
         if (req.user) {
             const user = req.user;
-            const gameState = {
-                players: [
-                    { id: 1, userID: 521, turnIndex: 4, username: "saggyballs", cards: 7, avatar: "https://store.playstation.com/store/api/chihiro/00_09_000/container/CH/de/99/EP2402-CUSA05624_00-AV00000000000213/0/image?_version=00_09_000&platform=chihiro&bg_color=000000&opacity=100&w=330&h=330" },
-                    { id: 2, userID: user.id, turnIndex: 7, username: user.username, cards: 2, avatar: user.pictureUrl },
-                    { id: 3, userID: 222, turnIndex: 6, username: "coolguy905", cards: 10, avatar: "https://store.playstation.com/store/api/chihiro/00_09_000/container/CH/de/99/EP2402-CUSA05624_00-AV00000000000213/0/image?_version=00_09_000&platform=chihiro&bg_color=000000&opacity=100&w=330&h=330" },
-                    { id: 4, userID: 319, turnIndex: 1, username: "joemomma", cards: 29, avatar: "https://store.playstation.com/store/api/chihiro/00_09_000/container/CH/de/99/EP2402-CUSA05624_00-AV00000000000213/0/image?_version=00_09_000&platform=chihiro&bg_color=000000&opacity=100&w=330&h=330" },
-                    { id: 5, userID: 401, turnIndex: 5, username: "horse666fivefive", cards: 29, avatar: "https://store.playstation.com/store/api/chihiro/00_09_000/container/CH/de/99/EP2402-CUSA05624_00-AV00000000000213/0/image?_version=00_09_000&platform=chihiro&bg_color=000000&opacity=100&w=330&h=330" },
-                    { id: 6, userID: 282, turnIndex: 3, username: "nike10", cards: 29, avatar: "https://store.playstation.com/store/api/chihiro/00_09_000/container/CH/de/99/EP2402-CUSA05624_00-AV00000000000213/0/image?_version=00_09_000&platform=chihiro&bg_color=000000&opacity=100&w=330&h=330" },
-                    { id: 7, userID: 808, turnIndex: 8, username: "tax", cards: 1, avatar: "https://store.playstation.com/store/api/chihiro/00_09_000/container/CH/de/99/EP2402-CUSA05624_00-AV00000000000213/0/image?_version=00_09_000&platform=chihiro&bg_color=000000&opacity=100&w=330&h=330" },
-                    { id: 8, userID: 913, turnIndex: 2, username: "king707", cards: 2, avatar: "https://store.playstation.com/store/api/chihiro/00_09_000/container/CH/de/99/EP2402-CUSA05624_00-AV00000000000213/0/image?_version=00_09_000&platform=chihiro&bg_color=000000&opacity=100&w=330&h=330" },
-                    { id: 9, userID: 121, turnIndex: 0, username: "glizzy", cards: 32, avatar: "https://store.playstation.com/store/api/chihiro/00_09_000/container/CH/de/99/EP2402-CUSA05624_00-AV00000000000213/0/image?_version=00_09_000&platform=chihiro&bg_color=000000&opacity=100&w=330&h=330" },
-                    { id: 10, userID: 552, turnIndex: 9, username: "flowerboy1", cards: 59, avatar: "https://store.playstation.com/store/api/chihiro/00_09_000/container/CH/de/99/EP2402-CUSA05624_00-AV00000000000213/0/image?_version=00_09_000&platform=chihiro&bg_color=000000&opacity=100&w=330&h=330" },
-                ],
-                playedDeck: [
-                    { id: 44, color: "red", value: 3, special: false },
-                    { id: 8, color: "yellow", value: 7, special: false },
-                    { id: 43, color: "blue", value: 7, special: false },
-                    { id: 56, color: "red", value: 4, special: false },
-                    { id: 1, color: "green", value: 0, special: false },
-                    { id: 38, color: "yellow", value: 6, special: false },
-                ],
-                mainDeck: [
-                    { id: 72, color: "green", value: 2, special: false },
-                    { id: 99, color: "yellow", value: 0, special: false },
-                    { id: 49, color: "red", value: 0, special: false },
-                    { id: 88, color: "red", value: 9, special: false },
-                    { id: 19, color: "green", value: 1, special: false },
-                    { id: 32, color: "blue", value: 6, special: false },
-                    { id: 44, color: "red", value: 3, special: false },
-                    { id: 8, color: "yellow", value: 7, special: false },
-                    { id: 43, color: "blue", value: 7, special: false },
-                    { id: 56, color: "red", value: 4, special: false },
-                    { id: 1, color: "green", value: 0, special: false },
-                    { id: 72, color: "blue", value: 2, special: false },
-                    { id: 99, color: "yellow", value: 0, special: false },
-                    { id: 49, color: "blue", value: 0, special: false },
-                ],
-                drawDeckCount: 12,
-                turnIndex: 0,
-                currentColor: "red",
-                playerOrderReversed: false,
-            };
             gameState.players.sort((a, b) => a.turnIndex - b.turnIndex);
             const mainPlayer = gameState.players.find(p => p.userID === user.id);
+            if (!mainPlayer) throw new Error("Unauthorized to join the game");
             const mainPlayerIndex = gameState.players.indexOf(mainPlayer);
             const players = [mainPlayer];
             for (let i = mainPlayerIndex; i < gameState.players.length; i++) {
@@ -98,7 +117,6 @@ router.delete('/:id/players', authenticate, async (req, res) => {
     try {
         if (req.user) {
             const user = req.user;
-
             res.json({ message: "Left the game" });
         } else res.status(401).json({ message: "Unauthorized" });
     } catch (err) {
@@ -111,29 +129,59 @@ router.patch('/:id/playCard', authenticate, async (req, res) => {
     try {
         if (req.user) {
             const user = req.user;
+            const mainPlayer = gameState.players.find(p => p.userID === user.id);
+            if (!mainPlayer) throw new Error("Unauthorized to join the game");
+            if (mainPlayer.turnIndex !== gameState.turnIndex) throw new Error("Unauthorized action, it is not your turn");
 
-            res.json({ gameState });
+            const id = parseInt(req.body.id);
+            const card = gameState.mainDeck.find(c => c.id === id);
+            gameState.mainDeck = gameState.mainDeck.filter(c => c !== card);
+            gameState.playedDeck.push(card);
+            gameState.players[0].cards = gameState.players[0].cards - 1;
+            gameState.currentColor = card.color;
+            // validate turn index
+            if (gameState.playerOrderReversed) {
+                if (gameState.turnIndex === 0) gameState.turnIndex = gameState.players.length - 1;
+                else gameState.turnIndex = gameState.turnIndex - 1;
+            } else {
+                if (gameState.turnIndex === gameState.players.length - 1) gameState.turnIndex = 0;
+                else gameState.turnIndex = gameState.turnIndex + 1;
+            }
+            res.json({ card, count: gameState.mainDeck.length, currentIndex: gameState.turnIndex ,success: true });
         } else res.status(401).json({ message: "Unauthorized" });
     } catch (err) {
         res.status(500).json({message: "Something went wrong"});
     }
 });
 
+let idCount = 100;
 /* Draw card */
 router.patch('/:id/drawCard', authenticate, async (req, res) => {
     try {
         if (req.user) {
             const user = req.user;
-
-            res.json({ gameState });
+            const mainPlayer = gameState.players.find(p => p.userID === user.id);
+            if (!mainPlayer) throw new Error("Unauthorized to join the game");
+            if (mainPlayer.turnIndex !== gameState.turnIndex) throw new Error("Unauthorized action, it is not your turn");
+            const card = { id: idCount, color: "green", value: 2, special: false };
+            idCount++;
+            if (gameState.drawDeckCount <= 0) throw new Error();
+            gameState.drawDeckCount = gameState.drawDeckCount - 1;
+            gameState.mainDeck.push(card);
+            gameState.players[0].cards = gameState.players[0].cards + 1;
+            if (gameState.playerOrderReversed) {
+                if (gameState.turnIndex === 0) gameState.turnIndex = gameState.players.length - 1;
+                else gameState.turnIndex = gameState.turnIndex - 1;
+            } else {
+                if (gameState.turnIndex === gameState.players.length - 1) gameState.turnIndex = 0;
+                else gameState.turnIndex = gameState.turnIndex + 1;
+            }
+            res.json({ card, count: gameState.mainDeck.length, currentIndex: gameState.turnIndex, success: true });
         } else res.status(401).json({ message: "Unauthorized" });
     } catch (err) {
         res.status(500).json({message: "Something went wrong"});
     }
 });
-
-
-
 
 const messages = [
     { id: 1, sender: "snowpuff808", message: "You're trash, kys ", createdAt: new Date() },
